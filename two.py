369c03fd507ce9505b17682a29830d6ccd6ae547
Murari Kumar <murariraman334@gmail.com>
2022-09-04 18:34:02 +0530

    revision coding of python
    revision coding of python

---
+++ two.py
@@ -0,0 +1,141 @@
+# second shitft practice of python coding
+# two special operators have been defined identity operator and membership op.
+# is -- called identity operator
+# in , not in ---- defined as membership operator
+
+# a=10222
+# b=10222
+
+# print(a is b)
+
+# c=20
+# d=40
+# print(c is d)
+# name='durga'
+# surname='durga'
+# print(name is surname)
+
+# identity operator 'is' is for address comparison not for content comparison
+# listone=[10,20,30]
+
+# listtwo=[10,20,30]
+
+# print( listone == listtwo)
+
+# membership operator here 
+
+# list1=[10,20,30,40,50]
+
+# print(10 in list1)
+
+# phrase= 'learning python is most important language here'
+# print('hello' not in phrase)
+# print('learning' in phrase)
+
+# operator precedence (), **, ~ -, 
+# identity operator -- is
+# membership operator -- in not in
+
+# x=input('enter any name')
+# print(type(x))
+# typecasting is compulsory in python3
+
+# first_number= int(input('enter the first number here'))
+# second_number=int( input('enter the second number here'))
+
+# print(first_number+second_number)
+# in any programming language the most commonly used data type is string 
+
+# read data from keyboard and print the data 
+# eno=int(input('enter employee number'))
+# ename=input('enter the employee name')
+# esal=float(input('enter the salary here'))
+# eaddress=input('enter the address of employee')
+# married=(input('enter whether married or not'))
+
+# print('emp number', eno)
+# print('name of employee', ename)
+# print('salary of employee is', esal)
+# print('address of employee is', eaddress)
+# print('marital staus is', married)
+# getting two numbers and splitting them using split function
+# a,b=[int(x) for x in input('enter two numbers').split(',')]
+# print('two numbers here are',a,b)
+# read two float values and print their sum here
+# a,b=[float(x) for x in input('enter floating value').split(',')]
+# print(a+b)
+
+# eval() to evaluate the functions
+
+# inputexpression='20+20+30+40+50+60'
+
+# outputresult=eval(inputexpression)
+# print(outputresult)
+# a,b,c=[eval(x) for x in input('enter three values here').split(',')]
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# a,b,c=[eval(x) for x in input('enter 3 values').split('5')]
+# print(type(a))
+# inputing data through commandline argument
+
+# from sys import argv
+# print(type(argv))
+
+# print(argv[1:])
+# import sys
+# print(sys.argv)
+# print(sys.argv[:])
+# print(sys.argv[1:])
+# print(len(sys.argv))
+
+# read a group of values from keyboard as commandline argument and print the
+# sum
+
+# from sys import argv
+
+# args=argv[1:]
+# sum1=0
+# for x in args:
+#     sum1=sum1+int(x)
+
+# print('the sum of values is', sum1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
